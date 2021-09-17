0dc8bd7183d81cab08a107f93d213a94d10edebf
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2021-09-17 12:28:49 -0300

    replace, split e substring

---
+++ Node/Node/.vscode/fundamentos/strings.js
@@ -0,0 +1,9 @@
+const nome = "Allan Santana 2810"
+
+console.log(nome.charAt(0))
+console.log(nome.charCodeAt(0))
+console.log(nome.indexOf("A"))
+console.log(nome.substring(1))
+console.log(nome.substring(0,6))
+console.log("Nome ".concat(nome).concat("!!!!"))
+console.log(nome.replace(/\d/g,"e"))
